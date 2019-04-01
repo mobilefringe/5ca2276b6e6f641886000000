ce23b4ae51b5d4a96f638c647da32e3fe52c6241
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-01 11:16:54 -0400

    Riverside 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                 social_media: [
                     {
                         name: "Facebook",
-                        url: "https://www.facebook.com",
+                        url: "https://www.facebook.com/riverside.plaza",
                         iconClass: "fab fa-facebook"
                     },
                     {

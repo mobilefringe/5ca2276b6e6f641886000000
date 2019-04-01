2a53830415c0007976be7458ea74cb104bc943da
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-01 12:12:41 -0400

    Riverside 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -21,7 +21,7 @@
                                     </div>
                                     <div class="col-sm-6" >
                                         <label for="fieldgjksd" class="accessibility">Last Name</label>
-                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldgjksd" name="cm-f-fihudi" type="text" placeholder="Last Name">
+                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldgjksd" name="cm-f-gjksd" type="text" placeholder="Last Name">
                                     </div>
                                 </div>
                                 <div class="row">

99671186d2afcdfb650c9806bd0e5c8436d920f2
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-09 11:09:16 -0400

    Riverside 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -77,6 +77,7 @@
                 return {
                     dataLoaded: true,
                     pageContent: null,
+                    pageBanner: null
                     siteInfo: site,
                     form_data : {},
                     formSuccess : false,

18e9e187d72f4e303dac8409387e9beb89364cac
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-09 11:12:11 -0400

    Riverside 2.0 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -37,7 +37,7 @@
                             <div class="col-md-12">
                                 <div class="row margin_30">
                                     <div class="col-md-12">
-                                        <router-link to="/events">
+                                        <router-link to="/events-and-promotions">
                     		                <div class="animated_btn pull-left">Back to Events</div>    
                     		            </router-link>    
                                     </div>

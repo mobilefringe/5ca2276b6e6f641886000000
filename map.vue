3ef60289011b191da6efd18489d890d7eb912bda
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-21 17:25:48 -0400

    Riverside Plaza 2.0 1.1  /map.vue

--- map.vue
+++ map.vue
@@ -106,7 +106,8 @@
                     return this.processedCategories;
                 },
                 dropDownCats() {
-                    var cats = _.map(this.processedCategories, 'name');
+                    // var cats = _.map(this.processedCategories, 'name');
+                    var cats = _.filter(this.processedCategories, function(o) { return o.name != "Dine Filter" && o.store_ids != null; });
                     cats.unshift('All');
                     return cats;
                 },

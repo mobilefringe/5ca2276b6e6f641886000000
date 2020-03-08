cecc0c11a7552fa05306b9ea711554ccc37c7c33
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-07 23:15:52 -0500

    Riverside Plaza 2.0 1.2  /main.js

--- main.js
+++ main.js
@@ -105,6 +105,10 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                             {
                                 name: "Dining",
                                 href: "/dine"
+                            },
+                            {
+                                name: "Delivery",
+                                href: "/delivery"
                             }
                         ]
                     },

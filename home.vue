54643c3ecc53eeb59f5f8e6929a3019bcc0331d3
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-03-31 15:20:55 -0400

    Riverside Plaza 2.0 1.2  /home.vue

--- home.vue
+++ home.vue
@@ -295,7 +295,7 @@
                             this.$store.dispatch("getData", "banners"), 
                             this.$store.dispatch("getData","promotions"), 
                             this.$store.dispatch("getData", "events"), 
-                            this.$store.dispatch('LOAD_PAGE_DATA', { url: "https://riverside.mallmaverick.com/api/v4/riverside/social.json" })
+                            this.$store.dispatch('LOAD_PAGE_DATA', { url: "https://riverside.mallmaverickstaging.com/api/v4/riverside/social.json" })
                         ]);
                         return results;
                     } catch(e) {
